37e0929bc21b588ebdaf7deaf5bf41f3940cb7b4
semyers189 <semyers@fullsail.edu>
2019-06-09 00:43:28 -0400

    moving repos state to context

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -26,6 +26,12 @@ export default (state, action) => {
         user: action.payLoad,
         loading: false
       };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: action.payLoad,
+        loading: false
+      };
     case SET_LOADING:
       return {
         ...state,
